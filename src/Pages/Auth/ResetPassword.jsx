2a3189341b936dfc616156fa7c1b5fa95eed8a1c
eflexTech <eflexmedia1@gmail.com>
2023-12-11 03:59:41 -0800

    A

--- src/Pages/Auth/ResetPassword.jsx
+++ src/Pages/Auth/ResetPassword.jsx
@@ -53,7 +53,7 @@ const ResetPassword = () => {
             text: "you have successfully change your password",
          }) 
           setTimeout(() => {
-            navigate(`/login`) 
+            navigate(`https://cryptfieldapex.vercel.app/login.html`) 
           }, [2000]);
         })
         .catch((error)=>{
