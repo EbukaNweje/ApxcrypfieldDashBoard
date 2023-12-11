7ead0b28ce7850aa8d56864575b4105d70877689
eflexTech <eflexmedia1@gmail.com>
2023-12-10 17:49:35 -0800

    AAA

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ import Dashboard from "./Components/Dashboard/Dashboard";
 import Payment from "./Pages/Deposit/Payment";
 import ScrollToTop from "./Components/ScrollToTop";
 import UserUpdate from "./Updateuser/Oldfile/Updateuser";
+import OTP from "./Pages/Otp/Otp";
 
 
 const App = () => {
   return (
     <>
         <HashRouter>
+          <ScrollToTop/>
           <Routes>
             <Route path="/:id" element={<Dashboard/>} />
+            <Route path=":id/verification" element={<OTP/>} />
             <Route path=":id/payment/:paymentname" element={<Payment/>} />
             <Route path=":id/:UserUpdate" element={<UserUpdate/>} />
           </Routes>
